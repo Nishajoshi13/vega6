11b06a8b6cfd5e7037d0f6399e8338ee36d6d2f1
Nishajoshi13 <nishajoshi132001@gmail.com>
2025-06-03 14:04:08 +0530

    minor change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,28 +15,28 @@ export class HomeComponent {
   cards:cards[] = [
     {
       desc:'Alex Soltan Is Making $23,000 Per Month Selling AI-Generated Ebooks…',
-      img: '/assets/home/card1.png',
+      img: 'assets/home/card1.png',
       
     },
     {
       desc:'Another Guy Has Made Over $1.63 Million By Selling AI-Generated Ebooks On Amazon…',
-      img: '/assets/home/card2.png',
+      img: 'assets/home/card2.png',
     },
     {
       desc:'This Guy Made An AI Chatbot That Makes Him $350,000 Each Year…',
-      img: '/assets/home/card3.png',
+      img: 'assets/home/card3.png',
     },
     {
       desc:'Jason Wardrop Sells AI Services Like Images, Videos, And Content To Local Businesses And Makes $19K/Month',
-      img: '/assets/home/card4.png',
+      img: 'assets/home/card4.png',
     },
     {
       desc:'This Guy Went From 0 To 56K Clicks In 3 Months Using AI Content (2025 Blog Case Study)',
-      img: '/assets/home/card5.png',
+      img: 'assets/home/card5.png',
     },
     {
       desc:'This Guy Started And Scaled His AI Agency To $122,351 Just To Prove It’s NOT Luck…',
-      img: '/assets/home/card6.png',
+      img: 'assets/home/card6.png',
     },
   ];
 
